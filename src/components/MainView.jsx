65846102b19b72ac53ac25747b50c47bf7a97d1a
becherryu <81617058+becherryu@users.noreply.github.com>
2024-11-18 20:46:39 +0900

    feat(MainView.jsx): 날짜 및 질문 표시

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,11 +1,38 @@
 import propTypes from "prop-types";
 import "./MainView.css";
+import { useEffect, useState } from "react";
 
 function MainView({ setView }) {
+  const [questions, setQuestions] = useState();
+  const now = new Date();
+
+  // 년/월/일 출력용
+  const date = now.getDate();
+  const month = now.getMonth() + 1;
+  const year = now.getFullYear();
+
+  // 첫 렌더링 시 한번만 출력하기
+  useEffect(() => {
+    // 질문 가져오기
+    fetch(
+      "https://raw.githubusercontent.com/hackurity01/simple-diary/main/src/questions.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data);
+      });
+  }, []);
+
+  if (!questions) {
+    return null;
+  }
+
   return (
     <>
       <div className="header">
-        <div>(오늘 날짜)</div>
+        <div>
+          {year}년 {month}월 {date}일
+        </div>
         <div>
           <button
             className="history-btn"
@@ -18,7 +45,7 @@ function MainView({ setView }) {
           </button>
         </div>
       </div>
-      <div className="question">(질문)</div>
+      <div className="question">{questions[date]}</div>
       <div className="content">
         <textarea
           onChange={() => {
